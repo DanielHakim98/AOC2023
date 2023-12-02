0a00bdf85a82fc945bdb1280d24653fdd3fb4ff0
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-12-02 09:16:26 +0800

    attempt on solving part 2 overlapping substring eightwothree

--- day1/calibrate.go
+++ day1/calibrate.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"unicode"
+	"strings"
 )
 
 func Day1() {
 	// 1. Open file
-	file, err := os.Open("./day1/input.txt")
+	file, err := os.Open("./day1/input_less.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,65 +32,55 @@ func Day1() {
 		log.Fatal(err)
 	}
 
-	// digitInLetters := map[string]string{
-	// 	"one":   "1",
-	// 	"two":   "2",
-	// 	"three": "3",
-	// 	"four":  "4",
-	// 	"five":  "5",
-	// 	"six":   "6",
-	// 	"seven": "7",
-	// 	"eight": "8",
-	// 	"nine":  "9",
-	// }
-	// keys := make([]string, 0)
-	// for k := range digitInLetters {
-	// 	keys = append(keys, k)
-	// }
-	// cleanedLines := make([]string, 0)
-	// for _, line := range lines {
-	// 	temp := line
-	// 	for _, key := range keys {
-	// 		if strings.Contains(temp, key) {
-	// 			fmt.Printf("line '%v' contains %v\n", temp, digitInLetters[key])
-	// 			v := strings.ReplaceAll(temp, key, digitInLetters[key])
-	// 			fmt.Println(v)
-	// 			temp = v
-	// 		}
-	// 	}
-	// 	fmt.Println("done: ", temp, "\n")
-	// 	cleanedLines = append(cleanedLines, temp)
-	// }
+	digitInLetters := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
 
-	// for _, v := range cleanedLines {
-	// 	fmt.Println(v)
-	// }
-	// 3. Get first occurence of first digit and last occurences of last digit
-	calibrators := make([]string, 0)
+	// cleanedLines := make([]string, 0)
 	for _, line := range lines {
-		temp := []string{}
-		for _, char := range line {
-			if unicode.IsDigit(char) {
-				temp = append(temp, string(char))
+		for k, v := range digitInLetters {
+			if strings.Contains(line, k) {
+				fmt.Printf("line '%v' contains '%v'\n", line, v)
+				fmt.Println(strings.ReplaceAll(line, k, v))
 			}
 		}
-		if len(temp) == 0 {
-			continue
-		} else if len(temp) == 1 {
-			// 3.1. If there is only 1 digit, then it's "<digit><digit>"
-			calibrators = append(calibrators, temp[0]+temp[0])
-		} else {
-			// 3.2. Concat first digit and last digit as "<first digit><last digit>"
-			calibrators = append(calibrators, temp[0]+temp[len(temp)-1])
-		}
+		fmt.Println()
 	}
 
-	// // 4. Sum of all calibrators
-	sum := 0
-	for _, calibrator := range calibrators {
-		v, _ := strconv.Atoi(calibrator)
-		sum += v
-	}
+	// // 3. Get first occurence of first digit and last occurences of last digit
+	// calibrators := make([]string, 0)
+	// for _, line := range cleanedLines {
+	// 	temp := []string{}
+	// 	for _, char := range line {
+	// 		if unicode.IsDigit(char) {
+	// 			temp = append(temp, string(char))
+	// 		}
+	// 	}
+	// 	if len(temp) == 0 {
+	// 		continue
+	// 	} else if len(temp) == 1 {
+	// 		// 3.1. If there is only 1 digit, then it's "<digit><digit>"
+	// 		calibrators = append(calibrators, temp[0]+temp[0])
+	// 	} else {
+	// 		// 3.2. Concat first digit and last digit as "<first digit><last digit>"
+	// 		calibrators = append(calibrators, temp[0]+temp[len(temp)-1])
+	// 	}
+	// }
 
-	fmt.Println("sum of calibration values: ", sum)
+	// // // 4. Sum of all calibrators
+	// sum := 0
+	// for _, calibrator := range calibrators {
+	// 	v, _ := strconv.Atoi(calibrator)
+	// 	sum += v
+	// }
+
+	// fmt.Println("sum of calibration values: ", sum)
 }
