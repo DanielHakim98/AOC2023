bfbc6f984d207b665cb2ca446fa997ed234c3c7a
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-12-02 11:23:39 +0800

    second attempt on solving part 2

--- day1/calibrate.go
+++ day1/calibrate.go
@@ -5,20 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-func Day1() {
+func GetInput() ([]string, error) {
 	// 1. Open file
 	file, err := os.Open("./day1/input_less.txt")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	defer func() {
-		if err = file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	defer file.Close()
 
 	// 2. Read line by line
 	scanner := bufio.NewScanner(file)
@@ -29,58 +27,87 @@ func Day1() {
 	}
 
 	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+var possibleVals = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+	"1":     "1",
+	"2":     "2",
+	"3":     "3",
+	"4":     "4",
+	"5":     "5",
+	"6":     "6",
+	"7":     "7",
+	"8":     "8",
+	"9":     "9",
+}
+
+type NumInLine struct {
+	Index int
+	Data  string
+}
+
+func (n NumInLine) String() string {
+	return fmt.Sprintf(`NumInLine{Index: %v,  Data: %v}`, n.Index, n.Data)
+}
+
+func Day1() {
+	lines, err := GetInput()
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	digitInLetters := map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-	}
-
-	// cleanedLines := make([]string, 0)
+	numbersInLines := make([][]NumInLine, 0)
 	for _, line := range lines {
-		for k, v := range digitInLetters {
+		occurences := make([]NumInLine, 0)
+		for k, v := range possibleVals {
 			if strings.Contains(line, k) {
-				fmt.Printf("line '%v' contains '%v'\n", line, v)
-				fmt.Println(strings.ReplaceAll(line, k, v))
+				start := strings.Index(line, k)
+				data := NumInLine{
+					Index: start,
+					Data:  v,
+				}
+				occurences = append(occurences, data)
 			}
 		}
-		fmt.Println()
+		sort.Slice(occurences, func(i, j int) bool {
+			return occurences[i].Index < occurences[j].Index
+		})
+		numbersInLines = append(numbersInLines, occurences)
 	}
 
-	// // 3. Get first occurence of first digit and last occurences of last digit
-	// calibrators := make([]string, 0)
-	// for _, line := range cleanedLines {
-	// 	temp := []string{}
-	// 	for _, char := range line {
-	// 		if unicode.IsDigit(char) {
-	// 			temp = append(temp, string(char))
-	// 		}
-	// 	}
-	// 	if len(temp) == 0 {
-	// 		continue
-	// 	} else if len(temp) == 1 {
-	// 		// 3.1. If there is only 1 digit, then it's "<digit><digit>"
-	// 		calibrators = append(calibrators, temp[0]+temp[0])
-	// 	} else {
-	// 		// 3.2. Concat first digit and last digit as "<first digit><last digit>"
-	// 		calibrators = append(calibrators, temp[0]+temp[len(temp)-1])
-	// 	}
-	// }
+	// 3. Get first occurence of first digit and last occurences of last digit
+	calibrators := make([]string, 0)
+	for _, line := range numbersInLines {
+		if len(line) == 0 {
+			continue
+		} else if len(line) == 1 {
+			// 3.1. If there is only 1 digit, then it's "<digit><digit>"
+			calibrators = append(calibrators, line[0].Data+line[0].Data)
+		} else {
+			// 3.2. Concat first digit and last digit as "<first digit><last digit>"
+			calibrators = append(calibrators, line[0].Data+line[len(line)-1].Data)
+		}
+	}
 
 	// // // 4. Sum of all calibrators
-	// sum := 0
-	// for _, calibrator := range calibrators {
-	// 	v, _ := strconv.Atoi(calibrator)
-	// 	sum += v
-	// }
+	sum := 0
+	for _, calibrator := range calibrators {
+		v, _ := strconv.Atoi(calibrator)
+		sum += v
+	}
 
-	// fmt.Println("sum of calibration values: ", sum)
+	fmt.Println("sum of calibration values: ", sum)
 }
