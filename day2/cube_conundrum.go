a2d8db0e5dcdeee544bea02019208855bdc0cf00
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-12-02 16:52:41 +0800

    add parse game record

--- day2/cube_conundrum.go
+++ day2/cube_conundrum.go
@@ -3,7 +3,10 @@ package day2
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func GetInput(filename string) ([]string, error) {
@@ -28,6 +31,52 @@ func GetInput(filename string) ([]string, error) {
 	return lines, nil
 }
 
+// only 12 red cubes, 13 green cubes, and 14 blue cubes?
+var bags = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
 func Day2(filename string, reader func(string) ([]string, error)) int {
+	games, err := reader(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g := ParseGame(games[0])
+	fmt.Println(g)
 	return 0
+}
+
+type Game struct {
+	Id   int
+	Sets []map[string]int
+}
+
+func ParseGame(game string) Game {
+	g := strings.Split(game, ":")
+
+	// Get Id
+	title := strings.Split(strings.TrimSpace(g[0]), " ")
+	id, _ := strconv.Atoi(title[len(title)-1])
+
+	// Collect set
+	collected := make([]map[string]int, 0)
+	sets := strings.Split(strings.TrimSpace(g[1]), "; ")
+	for _, set := range sets {
+		collect := make(map[string]int)
+		subsets := strings.Split(set, ", ")
+		for _, subset := range subsets {
+			s := strings.Split(subset, " ")
+			color := s[len(s)-1]
+			amount, _ := strconv.Atoi(s[0])
+			collect[color] = amount
+		}
+		collected = append(collected, collect)
+	}
+
+	return Game{
+		Id:   id,
+		Sets: collected,
+	}
 }
