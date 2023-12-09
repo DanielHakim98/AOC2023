92703f96ff24a1f2f69f2ca384b3fbb710cef174
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-12-09 14:19:04 +0800

    solved day5 part 1

--- day5/seed_fertilizer.go
+++ day5/seed_fertilizer.go
@@ -3,6 +3,9 @@ package day5
 import (
 	"fmt"
 	"log"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/DanielHakim98/aoc/utils"
 )
@@ -17,8 +20,53 @@ func (d *Day5) PartOne(filename string, reader utils.AocReader) int {
 		log.Fatal(err)
 	}
 
-	for _, line := range lines {
-		fmt.Println(string(line))
+	seeds := func() []int {
+		seedStr := strings.Fields(strings.Split(lines[0], ":")[1])
+		var seeds []int
+		for _, s := range seedStr {
+			num, _ := strconv.Atoi(s)
+			seeds = append(seeds, num)
+		}
+		return seeds
+	}()
+
+	results := make([]int, len(seeds))
+	copy(results, seeds)
+	done := make(map[int]int)
+	for _, line := range lines[1:] {
+		if len(line) == 0 {
+			clear(done)
+			// fmt.Println()
+			continue
+		}
+		if line[0] < '0' || line[0] > '9' {
+			// fmt.Println(line)
+			continue
+		}
+
+		srcDesMapper := strings.Fields(line)
+		ranges, _ := strconv.Atoi(srcDesMapper[2])
+		srcMin, _ := strconv.Atoi(srcDesMapper[1])
+		srcMax := srcMin + ranges - 1
+		desMin, _ := strconv.Atoi(srcDesMapper[0])
+		diff := desMin - srcMin
+
+		// fmt.Printf("src:[%v, %v] -> target[%v, %v]; diff: %v\n", srcMin, srcMax, desMin, desMin+ranges-1, diff)
+		for i, src := range results {
+			if src < srcMin || src > srcMax {
+				continue
+			}
+			if _, ok := done[src]; !ok {
+				target := src + diff
+				done[target] = src
+				// fmt.Println("source:", results[i])
+				// fmt.Println("target:", target)
+				results[i] = target
+			}
+
+		}
 	}
-	return 0
+
+	fmt.Println(results)
+	return slices.Min(results)
 }
--- day5/seed_fertilizer_test.go
+++ day5/seed_fertilizer_test.go
@@ -18,40 +18,71 @@ func TestDay5Part1(t *testing.T) {
 			reader: func(int, string) ([]string, error) {
 				return []string{
 					"seeds: 79 14 55 13",
-
+					"",
 					"seed-to-soil map:",
 					"50 98 2",
 					"52 50 48",
-
 					"soil-to-fertilizer map:",
 					"0 15 37",
 					"37 52 2",
 					"39 0 15",
-
+					"",
 					"fertilizer-to-water map:",
 					"49 53 8",
 					"0 11 42",
 					"42 0 7",
 					"57 7 4",
-
+					"",
 					"water-to-light map:",
 					"88 18 7",
 					"18 25 70",
-
+					"",
 					"light-to-temperature map:",
 					"45 77 23",
 					"81 45 19",
 					"68 64 13",
-
+					"",
 					"temperature-to-humidity map:",
 					"0 69 1",
 					"1 0 69",
-
+					"",
 					"humidity-to-location map:",
 					"60 56 37",
-					"56 93 4"}, nil
+					"56 93 4",
+				}, nil
 			},
 			want: 35,
+		},
+		{
+			// This is nasty because
+			// at fertilizer-to-water map,
+			// current seed 14 has soil 14, fertilizer 53,
+			// and 54 will be shifted to 49 based on this mapping
+			// but the catch is...49 is also matches in current map
+			// so previous logic will shift 49 to 38 despite having
+			// done mapping previously.
+			filename: "example_single",
+			reader: func(int, string) ([]string, error) {
+				return []string{
+					"seeds: 14",
+					"",
+					"seed-to-soil map:",
+					"50 98 2",
+					"52 50 48",
+					"",
+					"soil-to-fertilizer map:",
+					"0 15 37",
+					"37 52 2",
+					"39 0 15",
+					"",
+					"fertilizer-to-water map:",
+					"49 53 8",
+					"0 11 42",
+					"42 0 7",
+					"57 7 4",
+				}, nil
+			},
+			want: 49,
 		},
 	}
 
