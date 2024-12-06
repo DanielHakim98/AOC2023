c28fefc7ece575a056fff133eda6b914cc69a2d6
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-06 11:13:27 +0800

    day1: wip

--- 2024/src/days/day01.rs
+++ 2024/src/days/day01.rs
@@ -1,9 +1,25 @@
+use std::fs::read_to_string;
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
+    let file = read_to_string("input/day01_test.txt").expect("Failed to open input file");
+    let lines: Vec<&str> = file.trim().split("\n").collect();
+    let len = lines.len();
+    let mut left_vec: Vec<i32> = Vec::with_capacity(len);
+    let mut right_vec: Vec<i32> = Vec::with_capacity(len);
+
+    for line in lines {
+        let v: Vec<i32> = line
+            .split_whitespace()
+            .map(|e| e.parse::<i32>().unwrap_or_default())
+            .collect();
+        left_vec.push(v[0]);
+        right_vec.push(v[1]);
+    }
+
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
