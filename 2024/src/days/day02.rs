643d6e438da3d5bfd70e4ba3eb5f4bed5a73ab6a
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-07 13:37:49 +0800

    day2: attempt to solve part2

--- 2024/src/days/day02.rs
+++ 2024/src/days/day02.rs
@@ -4,7 +4,7 @@ use std::fs::read_to_string;
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    let file = read_to_string("inputs/day02_test.txt").expect("Failed to open input file");
+    let file = read_to_string("inputs/day02.txt").expect("Failed to open input file");
     let lines: Vec<&str> = file.trim().split("\n").collect();
 
     let sol1: u64 = count_report(&lines) as u64;
@@ -23,6 +23,7 @@ fn count_report_with_dampener(lines: &Vec<&str>) -> i32 {
 
         let mut is_increasing = true;
         let mut safe = true;
+        let mut unsafe_indexes = (0, 0);
         for (i, level) in report.iter().enumerate() {
             if i == 0 {
                 let next = report[i + 1];
@@ -37,6 +38,7 @@ fn count_report_with_dampener(lines: &Vec<&str>) -> i32 {
 
                 if is_divergent || is_out_of_range {
                     safe = false;
+                    unsafe_indexes = (i - 1, i);
                     break;
                 }
             }
@@ -44,10 +46,44 @@ fn count_report_with_dampener(lines: &Vec<&str>) -> i32 {
 
         if safe {
             total_safe += 1;
+        } else {
+            println!("{:?} , unsafe_indexes: {:?}", report, unsafe_indexes);
+            let mut report_no_prev = report.clone();
+            report_no_prev.remove(unsafe_indexes.0);
+            let mut report_no_cur = report.clone();
+            report_no_cur.remove(unsafe_indexes.1);
+            println!("{:?} {:?}", report_no_prev, report_no_cur);
+            if is_really_safe(&report_no_prev) || is_really_safe(&report_no_cur) {
+                total_safe += 1;
+            }
         }
     }
 
     total_safe
+}
+
+fn is_really_safe(report: &Vec<i32>) -> bool {
+    let mut is_increasing = true;
+    let mut safe = true;
+    for (i, level) in report.iter().enumerate() {
+        if i == 0 {
+            let next = report[i + 1];
+            let next_diff = next - level;
+            is_increasing = next_diff > 0;
+        } else {
+            let prev = report[i - 1];
+            let prev_diff = level - prev;
+
+            let is_divergent = is_increasing != (prev_diff > 0);
+            let is_out_of_range = prev_diff.abs() < 1 || prev_diff.abs() > 3;
+
+            if is_divergent || is_out_of_range {
+                safe = false;
+                break;
+            }
+        }
+    }
+    safe
 }
 
 fn count_report(lines: &Vec<&str>) -> i32 {
