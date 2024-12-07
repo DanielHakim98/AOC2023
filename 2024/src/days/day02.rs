c894f09b2d25c902f989e9dd3fedb9cdc5634bbf
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-07 12:59:18 +0800

    day2: wip part2

--- 2024/src/days/day02.rs
+++ 2024/src/days/day02.rs
@@ -4,8 +4,16 @@ use std::fs::read_to_string;
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    let file = read_to_string("inputs/day02.txt").expect("Failed to open input file");
+    let file = read_to_string("inputs/day02_test.txt").expect("Failed to open input file");
     let lines: Vec<&str> = file.trim().split("\n").collect();
+
+    let sol1: u64 = count_report(&lines) as u64;
+    let sol2: u64 = count_report_with_dampener(&lines) as u64;
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn count_report_with_dampener(lines: &Vec<&str>) -> i32 {
     let mut total_safe = 0;
     for line in lines {
         let report: Vec<i32> = line
@@ -16,66 +24,64 @@ pub fn solve() -> SolutionPair {
         let mut is_increasing = true;
         let mut safe = true;
         for (i, level) in report.iter().enumerate() {
-            // println!("{} {}", i, level);
             if i == 0 {
-                // println!("compare with next element");
                 let next = report[i + 1];
                 let next_diff = next - level;
                 is_increasing = next_diff > 0;
-
-                // Check difference
-                if next_diff.abs() < 1 || next_diff.abs() > 3 {
-                    safe = false;
-                    break;
-                }
-            } else if i == report.len() - 1 {
-                // println!("compare with previous element")
-                let prev = report[i - 1];
-                let prev_diff = level - prev;
-                let is_still_increasing = prev_diff > 0;
-
-                // Check increasing
-                if is_increasing != is_still_increasing {
-                    safe = false;
-                    break;
-                }
-
-                // Check difference
-                if prev_diff.abs() < 1 || prev_diff.abs() > 3 {
-                    safe = false;
-                    break;
-                }
             } else {
-                // println!("compare with next and previous element");
-                let next = report[i + 1];
                 let prev = report[i - 1];
-                let next_diff = next - level;
                 let prev_diff = level - prev;
-                // Check increasing
-                let is_still_increasing = prev_diff > 0;
-                if is_increasing != is_still_increasing {
-                    safe = false;
-                    break;
-                }
 
-                // Check difference
-                if next_diff.abs() < 1 || next_diff.abs() > 3 {
-                    safe = false;
-                    break;
-                }
-                if prev_diff.abs() < 1 || prev_diff.abs() > 3 {
+                let is_divergent = is_increasing != (prev_diff > 0);
+                let is_out_of_range = prev_diff.abs() < 1 || prev_diff.abs() > 3;
+
+                if is_divergent || is_out_of_range {
                     safe = false;
                     break;
                 }
             }
         }
+
         if safe {
             total_safe += 1;
         }
     }
 
-    let sol1: u64 = total_safe as u64;
-    let sol2: u64 = 0;
+    total_safe
+}
 
-    (Solution::from(sol1), Solution::from(sol2))
+fn count_report(lines: &Vec<&str>) -> i32 {
+    let mut total_safe = 0;
+    for line in lines {
+        let report: Vec<i32> = line
+            .split_whitespace()
+            .map(|e| e.parse::<i32>().unwrap())
+            .collect();
+
+        let mut is_increasing = true;
+        let mut safe = true;
+        for (i, level) in report.iter().enumerate() {
+            if i == 0 {
+                let next = report[i + 1];
+                let next_diff = next - level;
+                is_increasing = next_diff > 0;
+            } else {
+                let prev = report[i - 1];
+                let prev_diff = level - prev;
+
+                let is_divergent = is_increasing != (prev_diff > 0);
+                let is_out_of_range = prev_diff.abs() < 1 || prev_diff.abs() > 3;
+
+                if is_divergent || is_out_of_range {
+                    safe = false;
+                    break;
+                }
+            }
+        }
+
+        if safe {
+            total_safe += 1;
+        }
+    }
+    total_safe
 }
