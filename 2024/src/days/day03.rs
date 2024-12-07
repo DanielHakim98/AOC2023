c362fe4672dcbb5c42a55b63e21dcc4f59299116
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-07 21:49:55 +0800

    day03: setup day03

---
+++ 2024/src/days/day03.rs
@@ -0,0 +1,14 @@
+use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn solve() -> SolutionPair {
+    // let file = read_to_string("inputs/day03.txt").expect("Failed to open input file");
+    // let lines: Vec<&str> = file.trim().split("\n").collect();
+
+    let sol1: u64 = 0 as u64;
+    let sol2: u64 = 0 as u64;
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
