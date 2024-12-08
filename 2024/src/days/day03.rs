05a5d3dd549e01788db9855d243ae607f2a89a1c
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-08 09:13:44 +0800

    day03: refractor part1

--- 2024/src/days/day03.rs
+++ 2024/src/days/day03.rs
@@ -8,25 +8,29 @@ pub fn solve() -> SolutionPair {
     let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
     let file = read_to_string("inputs/day03.txt").expect("Failed to open input file");
     let lines: Vec<&str> = file.trim().split("\n").collect();
-    let mut total = 0;
-    for line in lines {
-        for m in re.captures_iter(line) {
-            if let Some(v) = m.get(0) {
-                let mul_exp = v
-                    .as_str()
-                    .replace("mul", "")
-                    .replace("(", "")
-                    .replace(")", "");
-                let num = mul_exp
-                    .split(",")
-                    .map(|e| e.parse::<i32>().unwrap())
-                    .fold(1, |acc, x| acc * x);
-                total += num;
-            }
-        }
-    }
-    let sol1: u64 = total as u64;
+
+    let sol1: u64 = mull_it_over_part_1(&re, &lines) as u64;
     let sol2: u64 = 0 as u64;
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn mull_it_over_part_1(re: &Regex, lines: &Vec<&str>) -> i32 {
+    let mut total = 0;
+    for line in lines {
+        for m in re.captures_iter(line) {
+            total += match m.get(0) {
+                Some(v) => v
+                    .as_str()
+                    .replace("mul", "")
+                    .replace("(", "")
+                    .replace(")", "")
+                    .split(",")
+                    .map(|e| e.parse::<i32>().unwrap())
+                    .fold(1, |acc, x| acc * x),
+                _ => 0,
+            }
+        }
+    }
+    total
 }
