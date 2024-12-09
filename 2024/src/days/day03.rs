957b1753be94412c5d1a90ed16e745d9a714e75c
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-09 15:43:11 +0800

    day03: attempt to solve day3

--- 2024/src/days/day03.rs
+++ 2024/src/days/day03.rs
@@ -1,19 +1,16 @@
 use crate::{Solution, SolutionPair};
 use regex::Regex;
-use std::{
-    fs::{read_to_string, File},
-    io::{self, Read},
-};
+use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
     let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
-    let file = read_to_string("inputs/day03.txt").expect("Failed to open input file");
+    let file = read_to_string("inputs/day03_test.txt").expect("Failed to open input file");
     let lines: Vec<&str> = file.trim().split("\n").collect();
 
     let sol1: u64 = mull_it_over_part_1(&re, &lines) as u64;
-    let sol2: u64 = mull_it_over_part_2() as u64;
+    let sol2: u64 = mull_it_over_part_2(&file.trim()) as u64;
 
     (Solution::from(sol1), Solution::from(sol2))
 }
@@ -38,29 +35,69 @@ fn mull_it_over_part_1(re: &Regex, lines: &Vec<&str>) -> i32 {
     total
 }
 
-fn mull_it_over_part_2() -> i32 {
-    let file = File::open("inputs/day03_test.txt").unwrap();
-    let mut reader = io::BufReader::new(file);
-    let mut buf = [0; 10];
-    loop {
-        let bytes_read = reader.read(&mut buf).unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        let chunk_str = std::str::from_utf8(&buf[..bytes_read]).unwrap_or_default();
-        for ch in chunk_str.chars() {
-            println!("{:?}", ch);
-        }
-        println!("{:?}", chunk_str);
-    }
-    0
-}
+fn mull_it_over_part_2(content: &str) -> i32 {
+    let len = content.len();
+    let mut i = 0;
+    let mut enable = true;
+    while i < len {
+        let c = content.chars().nth(i).unwrap_or_default();
 
-#[derive(Debug)]
-enum Token {
-    Identifier(String),
-    Number(i32),
-    LeftParenthesis,
-    RightParenthesis,
-    Comma,
+        match c {
+            'd' => {
+                if let Some(next) = content.get(i..i + 4) {
+                    if next == "do()" {
+                        if !enable {
+                            enable = true
+                        }
+                        i += 4;
+                        continue;
+                    }
+                }
+                if let Some(next) = content.get(i..i + 7) {
+                    if next == "don't()" {
+                        if enable {
+                            enable = false
+                        }
+                        i += 7;
+                        continue;
+                    }
+                }
+            }
+            'm' => {
+                if let Some(next) = content.get(i..i + 4) {
+                    if next == "mul(" {
+                        i += 4;
+                        let mut j = i;
+                        let mut char = content.chars().nth(j).unwrap_or_default();
+                        while char != ',' {
+                            j += 1;
+                            char = content.chars().nth(j).unwrap_or_default();
+                        }
+                        if let Some(nums) = content.get(i..j) {
+                            let first = nums.parse::<i32>().unwrap_or(1);
+                            println!("first: {first} i: {i} j: {j}");
+                        }
+
+                        i = j + 1;
+                        println!("i:{i}");
+                        let mut k = i;
+                        let mut char = content.chars().nth(k).unwrap_or_default();
+                        while char != ')' {
+                            k += 1;
+                            char = content.chars().nth(k).unwrap_or_default();
+                        }
+                        if let Some(nums) = content.get(i..k) {
+                            println!("nums:{nums}\n");
+                        }
+                        continue;
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        i += 1;
+    }
+
+    0
 }
