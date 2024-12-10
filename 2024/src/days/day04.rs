95047c1a2ef90ff5102c181e8f4d16661fbf4da1
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-10 11:25:07 +0800

    day04: setup day4

---
+++ 2024/src/days/day04.rs
@@ -0,0 +1,8 @@
+use crate::{Solution, SolutionPair};
+
+pub fn solve() -> SolutionPair {
+    let sol1: u64 = 0 as u64;
+    let sol2: u64 = 0 as u64;
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
