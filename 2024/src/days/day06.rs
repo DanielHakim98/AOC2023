d4de793a7a3dec98e0a8c4c68c20ca823c90a20f
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-14 11:17:35 +0800

    day06: setup

---
+++ 2024/src/days/day06.rs
@@ -0,0 +1,11 @@
+use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
+
+pub fn solve() -> SolutionPair {
+    let file = read_to_string("inputs/day06_test.txt").expect("Failed to open input file");
+
+    let sol1: u64 = 0 as u64;
+    let sol2: u64 = 0 as u64;
+
+    (Solution::from(sol1), Solution::from(sol2))
+}
