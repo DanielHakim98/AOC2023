770718857d50d83c6d473e0178b42f76c4f39a23
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-12-15 11:40:07 +0800

    day06: still wip part 1

--- 2024/src/days/day06.rs
+++ 2024/src/days/day06.rs
@@ -1,8 +1,8 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 pub fn solve() -> SolutionPair {
-    let file = read_to_string("inputs/day06_test.txt").expect("Failed to open input file");
+    let file = read_to_string("inputs/day06.txt").expect("Failed to open input file");
     let sol1: u64 = part1(&file) as u64;
     let sol2: u64 = 0 as u64;
 
@@ -37,79 +37,17 @@ fn change_direction(cur_direction: &Direction) -> Direction {
     }
 }
 
-fn move_direction(guard_position: &mut Position, lines: &mut Vec<Vec<char>>) {
+fn move_direction(guard_position: &Position) -> (i32, i32) {
     match &guard_position.direction {
-        up_or_down @ (Direction::Up | Direction::Down) => {
-            let cal_next_move = |cur_move: i32| -> i32 {
-                match up_or_down {
-                    Direction::Up => cur_move - 1,
-                    _ => cur_move + 1,
-                }
-            };
-            let guard_pos_row = guard_position.row as i32;
-            let guard_pos_col = guard_position.col as usize;
-            let mut next_move = cal_next_move(guard_pos_row);
-
-            while next_move > 0 && lines[next_move as usize][guard_pos_col] != '#' {
-                println!("next_move: {next_move}");
-
-                // swap
-                let temp = lines[guard_position.row as usize][guard_pos_col];
-                lines[guard_position.row as usize][guard_pos_col] =
-                    lines[next_move as usize][guard_pos_col];
-                lines[next_move as usize][guard_pos_col] = temp;
-
-                // update guard_position
-                guard_position.row = next_move;
-
-                // show current location
-                println!("=========");
-                for line in &mut *lines {
-                    println!("{:?}", line);
-                }
-                println!("=========");
-
-                next_move = cal_next_move(guard_position.row);
-            }
-        }
-        left_or_right @ (Direction::Left | Direction::Right) => {
-            let cal_next_move = |cur_move: i32| -> i32 {
-                match left_or_right {
-                    Direction::Left => cur_move - 1,
-                    _ => cur_move + 1,
-                }
-            };
-            let guard_pos_row = guard_position.row as usize;
-            let guard_pos_col = guard_position.col as i32;
-            let mut next_move = cal_next_move(guard_pos_col);
-            while next_move < (lines[0].len() as i32)
-                && lines[guard_pos_row][next_move as usize] != '#'
-            {
-                println!("next_move: {next_move}");
-
-                let temp = lines[guard_pos_row][guard_pos_col as usize];
-                lines[guard_pos_row][guard_pos_col as usize] =
-                    lines[guard_pos_row][next_move as usize];
-                lines[guard_pos_row][next_move as usize] = temp;
-
-                // update guard position
-                guard_position.col = next_move;
-
-                // show current location
-                println!("=========");
-                for line in &mut *lines {
-                    println!("{:?}", line);
-                }
-                println!("=========");
-
-                next_move = cal_next_move(guard_position.col);
-            }
-        }
+        Direction::Up => (-1, 0),
+        Direction::Right => (0, 1),
+        Direction::Down => (1, 0),
+        Direction::Left => (0, -1),
     }
 }
 
 fn part1(content: &str) -> usize {
-    let mut lines: Vec<Vec<char>> = content
+    let lines: Vec<Vec<char>> = content
         .trim()
         .split("\n")
         .map(|line| line.chars().collect::<Vec<char>>())
@@ -135,14 +73,28 @@ fn part1(content: &str) -> usize {
             break;
         }
     }
-    println!("{:?}", guard_position);
 
-    while guard_position.col > 0 && guard_position.col < max_col as i32
-        || guard_position.row > 0 && guard_position.row < max_row as i32
+    let mut step_history: HashMap<(i32, i32), usize> = HashMap::new();
+    while guard_position.row > 0
+        && guard_position.row < (max_row - 1) as i32
+        && guard_position.col > 0
+        && guard_position.col < (max_col - 1) as i32
     {
-        move_direction(&mut guard_position, &mut lines);
-        guard_position.direction = change_direction(&guard_position.direction);
-    }
+        let mut next_row_pos = guard_position.row + move_direction(&guard_position).0;
+        let mut next_col_pos = guard_position.col + move_direction(&guard_position).1;
 
-    0
+        if lines[next_row_pos as usize][next_col_pos as usize] == '#' {
+            guard_position.direction = change_direction(&guard_position.direction);
+            next_row_pos = guard_position.row + move_direction(&guard_position).0;
+            next_col_pos = guard_position.col + move_direction(&guard_position).1;
+        }
+
+        guard_position.row = next_row_pos;
+        guard_position.col = next_col_pos;
+
+        let coord = (guard_position.row, guard_position.col);
+        *step_history.entry(coord).or_default() += 1;
+    }
+    // println!("{:#?}", step_history);
+    step_history.len()
 }
