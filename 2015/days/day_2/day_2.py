a59e1f03598078b02cba4ea452ec70d813f996ab
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2025-01-01 10:24:06 +0800

    2015-day02: solve part 1 and part 2

--- 2015/days/day_2/day_2.py
+++ 2015/days/day_2/day_2.py
@@ -2,18 +2,43 @@ FILENAME = "days/day_2/day_2.txt"
 
 
 def part_1(path: str) -> int:
+    total = 0
     with open(path) as f:
-        content = f.read()
-    print(content)
-    return 0
+        content = f.readlines()
+
+        for line in content:
+            dimensions = [int(item) for item in line.strip().split("x")]
+            leng = dimensions[0]
+            widt = dimensions[1]
+            heig = dimensions[2]
+
+            leng_widt = leng * widt
+            widt_heig = widt * heig
+            heig_leng = heig * leng
+            min_surface = min(leng_widt, widt_heig, heig_leng)
+            area = 2 * leng_widt + 2 * widt_heig + 2 * heig_leng + min_surface
+            total += area
+    return total
 
 
 def part_2(path: str) -> int:
+    total = 0
     with open(path) as f:
-        content = f.read()
-    print(content)
-    floor = 0
-    return floor
+        content = f.readlines()
+        for line in content:
+            dimensions = [int(item) for item in line.strip().split("x")]
+            leng = dimensions[0]
+            widt = dimensions[1]
+            heig = dimensions[2]
+
+            leng_widt = 2 * leng + 2 * widt
+            widt_heig = 2 * widt + 2 * heig
+            heig_leng = 2 * heig + 2 * leng
+            min_peri = min(leng_widt, widt_heig, heig_leng)
+            ribbon_feet = min_peri + leng * widt * heig
+            total += ribbon_feet
+
+    return total
 
 
 if __name__ == "__main__":
